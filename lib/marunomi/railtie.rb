9026f93862ab31d85feaa279b1d42902f20ea353
takahashi <>
2018-08-30 12:36:23 +0900

    delete action view

--- lib/marunomi/railtie.rb
+++ lib/marunomi/railtie.rb
@@ -1,9 +1,9 @@
 module Marunomi
   class Railtie < Rails::Railtie
-    initializer 'marunomi' do |app|
-      ActiveSupport.on_load :action_view do
-        require 'my_plugin/view_helpers/action_view'
-      end
-    end
+    # initializer 'marunomi' do |app|
+    #   ActiveSupport.on_load :action_view do
+    #     require 'my_plugin/view_helpers/action_view'
+    #   end
+    # end
   end
 end
