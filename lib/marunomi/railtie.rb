1fa3e1d9e2623cdb7dc1f7d7fc08d50b09831bf3
takahashi <>
2018-08-30 12:35:20 +0900

    fix module name

--- lib/marunomi/railtie.rb
+++ lib/marunomi/railtie.rb
@@ -1,9 +1,9 @@
-module MyPlugin
+module Marunomi
   class Railtie < Rails::Railtie
-    # initializer 'my_plugin' do |app|
-    #   ActiveSupport.on_load :action_view do
-    #     require 'my_plugin/view_helpers/action_view'
-    #   end
-    # end
+    initializer 'marunomi' do |app|
+      ActiveSupport.on_load :action_view do
+        require 'my_plugin/view_helpers/action_view'
+      end
+    end
   end
 end
