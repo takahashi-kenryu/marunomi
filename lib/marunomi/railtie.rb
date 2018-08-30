50bf6cd8fe7451ff7832669a4edea6ba0db47992
takahashi <>
2018-08-30 12:28:19 +0900

    railtie

---
+++ lib/marunomi/railtie.rb
@@ -0,0 +1,9 @@
+module MyPlugin
+  class Railtie < Rails::Railtie
+    # initializer 'my_plugin' do |app|
+    #   ActiveSupport.on_load :action_view do
+    #     require 'my_plugin/view_helpers/action_view'
+    #   end
+    # end
+  end
+end
